07c53758d307a83e744bdb2e8f9b54f849e9512b
WILSON-HOME\WilsonHuang <hjn201@gmail.com>
2017-03-22 23:45:54 +0800

    練習

--- src/com/dao/GenericHibernateDAO.java
+++ src/com/dao/GenericHibernateDAO.java
@@ -1,8 +1,16 @@
 package com.dao;
 
+import com.util.HibernateUtil;
+
+import org.hibernate.Criteria;
 import org.hibernate.Session;
+import org.hibernate.criterion.Criterion;
+import org.hibernate.criterion.Example;
+import org.hibernate.query.Query;
 
 import java.io.Serializable;
+import java.util.List;
+
 
 /**
  * Created by ki264 on 2017/3/22.
@@ -19,6 +27,83 @@ public class GenericHibernateDAO<T, ID extends Serializable> {
     }
 
     protected Session getSession() {
-        return null;
+        return HibernateUtil.getSessionFactory().getCurrentSession();
     }
+
+    public List<T> getByHsql(String hsql) {
+        Query query = getSession().createQuery(hsql);
+        return query.list();
+    }
+
+    protected List<T> getByCriteria(Criterion... criteria) {
+        Criteria crit = getSession().createCriteria(getPersistentClass());
+        for (Criterion c : criteria) {
+            crit.add(c);
+        }
+        return crit.list();
+    }
+
+    public T getById(ID id) {
+        T entity = (T) getSession().get(getPersistentClass(), id);
+        return entity;
+    }
+
+    public List<T> getAll() {
+        return getByCriteria();
+    }
+
+    public List<T> getByExample(T exampleInstance) {
+        return getByCriteria(Example.create(exampleInstance));
+    }
+
+    public T makePersistent(T entity) {
+        getSession().saveOrUpdate(entity);
+        return entity;
+    }
+
+    public void makeTransient(T entity) {
+        getSession().delete(entity);
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/com/util/HibernateUtil.java
+++ src/com/util/HibernateUtil.java
@@ -1,6 +1,12 @@
 package com.util;
 
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+
 /**
  * Created by ki264 on 2017/3/22.
  * <p>
@@ -8,6 +14,58 @@ package com.util;
  * 透過靜態的初始化程式碼來讀取Hibernate啟動參數，並初始化Configuration和SessionFactory對象。
  */
 public class HibernateUtil {
+    private static Log log = LogFactory.getLog(HibernateUtil.class);
 
+    private static Configuration configuration;
+    private static SessionFactory sessionFactory;
 
+    static {
+        try {
+            configuration = new Configuration();
+            configuration.configure();
+            sessionFactory = configuration.buildSessionFactory();
+        } catch (Throwable ex) {
+            log.error("建立SessionFactory失敗", ex);
+            ex.printStackTrace();
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static Configuration getConfiguration() {
+        return configuration;
+    }
+
+    public static SessionFactory getSessionFactory() {
+        SessionFactory sf = sessionFactory;
+        if (sf == null)
+            throw new IllegalStateException("SessionFactory不可呼叫");
+
+        return sf;
+    }
+
+    public static void shutdown() {
+        log.debug("Shutting down Hibernate.");
+        getSessionFactory().close();
+
+        configuration = null;
+        sessionFactory = null;
+
+    }
+
+    public static void commitAndBeginTransaction() {
+        sessionFactory.getCurrentSession().getTransaction().commit();
+        sessionFactory.getCurrentSession().beginTransaction();
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
